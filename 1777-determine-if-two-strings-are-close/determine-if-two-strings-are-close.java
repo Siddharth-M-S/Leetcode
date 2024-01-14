272c220c3c86703ae91ab9146b59101ba0a504ec
Siddharth-M-S <96465030+Siddharth-M-S@users.noreply.github.com>
2024-01-14 19:26:21 +0530

    Time: 10 ms (92.33%) | Memory: 45.6 MB (27.19%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] a = new int[26];
+        int[] b = new int[26];
+
+        for (int i = 0; i < word1.length(); ++i) {
+            a[word1.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < word2.length(); ++i) {
+            b[word2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if ((a[i] > 0 && b[i] == 0) || (b[i] > 0 && a[i] == 0)) {
+                return false; 
+            }
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i = 0; i < 26; ++i) {
+            if (a[i] != b[i]) {
+                return false; 
+            }
+        }
+        return true;
+    }
+}
