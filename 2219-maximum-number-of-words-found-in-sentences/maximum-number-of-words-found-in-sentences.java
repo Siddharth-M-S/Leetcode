1ec2501a1836281019a40a5e32aea24e4cffb53f
Siddharth-M-S <96465030+Siddharth-M-S@users.noreply.github.com>
2024-01-26 19:30:29 +0530

    Time: 10 ms (9.91%) | Memory: 44.6 MB (12.95%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -3,36 +3,16 @@ class Solution {
         int max =0;
         for(int i=0;i<sentences.length;i++)
         {
-            // System.out.println(sentences[i]);
-            // System.out.print(sentences[i].length());
+            
             String arr[]= sentences[i].split("[ ]");
             int len = arr.length;
-            // if(len>max)
-            // {
-            //     max=len;
-            // }
-
+         
             max=Math.max(max,len);
         }
 
     
-        // int count =0;
-        // for(int j =0;j<sentences[i].length();j++)
-        // {
-        //     if(sentences[i].charAt(j)==' ')
-        //     {
-        //         count++;
-        //     }
-            
-        // }
-        // int k = count+1;
-        // if(k>max)
-        // {
-        //     max=k;
-        // }
-        // // System.out.print(count);
-
-        return max;
+        
+                return max;
         }
 
     }
